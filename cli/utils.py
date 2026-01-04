e9f577d15f8a0178ea3d2b688118ed58006a68da
gooneraki <hayiate4@hotmail.com>
2026-01-04 18:21:35 +0200

    Add stemming to query and movie title processing
    Integrates the PorterStemmer from NLTK to normalize words in
    queries and movie titles by reducing them to their stem forms.
    This enhancement improves search result relevance by matching
    words with similar roots.
    
    Adds the NLTK library as a dependency in the project
    configuration files.

--- cli/utils.py
+++ cli/utils.py
@@ -1,4 +1,7 @@
 import string
+from nltk.stem import PorterStemmer
+
+stemmer = PorterStemmer()
 
 def is_any_word_in_words(sourceWords, targetWords):
     for word in sourceWords:
@@ -18,11 +21,13 @@ def get_search_results(query, movieContents, stopWords):
 
     queryClean = query.translate(translator).lower()
     queryWords = [x for x in queryClean.split(" ") if len(x) > 0 and x not in stopWords]
+    queryWords = [stemmer.stem(word) for word in queryWords]
 
     for movie in movieContents:
         movieTitle = movie["title"]
         movieTitleClean = movieTitle.translate(translator).lower()
         movieTitleWords = [x for x in movieTitleClean.split(" ") if len(x) > 0 and x not in stopWords]
+        movieTitleWords = [stemmer.stem(word) for word in movieTitleWords]
 
         if is_any_word_in_words(queryWords, movieTitleWords):
             result_list.append(movieTitle)
