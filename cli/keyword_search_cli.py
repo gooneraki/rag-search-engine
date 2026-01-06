daeb585656e35df235155edd87ea3c06452ea5b1
gooneraki <hayiate4@hotmail.com>
2026-01-06 11:34:01 +0200

    Add IDF calculation command to keyword search CLI

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
+import math
 
-from utils import read_stop_words, get_search_results, clean_words
-from inverted_index import InvertedIndex
+from utils import read_stop_words, clean_words
+from inverted_index import InvertedIndex,tokenize_text
 
 
 def main() -> None:
@@ -18,6 +18,9 @@ def main() -> None:
     term_parser = subparsers.add_parser("tf", help="Get term frequency in a document")
     term_parser.add_argument("doc_id", type=int, help="Document ID")
     term_parser.add_argument("term", type=str, help="Term to search for")
+
+    idf_parser = subparsers.add_parser("idf", help="Get inverse document frequency of a term")
+    idf_parser.add_argument("term", type=str, help="Term to search for")
 
     args = parser.parse_args()
 
@@ -50,6 +53,32 @@ def main() -> None:
             index = InvertedIndex()
             index.build()
             index.save()
+
+        case "idf":
+            index = InvertedIndex()
+            
+            try:
+                index.load()
+            except FileNotFoundError as e:
+                print(f"Error: {e}")
+                return
+            
+            
+            tokenized_term = tokenize_text(args.term)
+            if len(tokenized_term) > 1:
+                print("Error: Please provide a single term for IDF calculation.")
+                return
+            elif len(tokenized_term) == 0:
+                print(f"Error: No valid term provided after tokenization for '{args.term}'.")
+                return
+
+            term = tokenized_term[0]
+            doc_ids = index.get_documents(term)
+            total_doc_count = len(index.docmap)
+            term_match_doc_count = len(doc_ids)
+
+            idf = math.log((total_doc_count + 1) / (term_match_doc_count + 1))
+            print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
 
         case "tf":
             index = InvertedIndex()
